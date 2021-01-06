cf9eb25eeec83fb42a0c7c5835a355cd7cd1cf74
moisescualexandru <alexandrumoisescu@MacBook-Pro.local>
2021-01-06 14:08:30 +0200

    added boiler-plate for initial commit vuex-store

---
+++ store/index.js
@@ -0,0 +1,17 @@
+export const state = () => ({
+    recipes: {},
+    ingredients: {}
+});
+
+export const getters = {
+    getRecipes: (state) => (state.recipes),
+    ingredients: (state) => (state.ingredients)
+};
+
+export const actions = {
+
+};
+
+export const mutations = {
+
+};
