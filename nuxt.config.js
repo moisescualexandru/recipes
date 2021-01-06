7d70cfaaa1737da5721f5bc3359384b9da99c282
Alexandru Moisescu <alexandrumoisescu@MacBook-Pro.local>
2021-01-06 12:39:50 +0200

    initial commit - boiler plate

---
+++ nuxt.config.js
@@ -0,0 +1,85 @@
+import path from 'path';
+import fs from 'fs';
+
+export default {
+  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
+  ssr: false,
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'recipes',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module'
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+    //https://i18n.nuxtjs.org
+    'nuxt-i18n'
+  ],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {},
+
+  // Content module configuration (https://go.nuxtjs.dev/config-content)
+  content: {},
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  },
+  // Auto compile components
+  components: true,
+  // Https server config for
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'server.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'server.crt'))
+    }
+  },
+  // Nuxt internationalization configuration
+  i18n: {
+    baseUrl: 'https://localhost:3000/',
+    locales: [
+      { code: 'en', iso: 'en', file: 'en.js' },
+      { code: 'ro', iso: 'ro', file: 'ro.js' }
+    ],
+    defaultLocale: 'ro',
+    lazy: true,
+    strategy: 'prefix',
+    langDir: 'assets/lang/',
+    detectBrowserLanguage: {
+      fallbackLocale: 'ro',
+      onlyOnRoot: true,
+      cookieKey: 'lang-set'
+    }
+  }
+}
