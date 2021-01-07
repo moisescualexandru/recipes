7fa11452d3d5cd1e54f0fab8b429579c68e8630f
moisescualexandru <alexandrumoisescu@MacBook-Pro.local>
2021-01-07 10:54:54 +0200

    added schemas for ingredient and ingredientWithQty

--- server/schemas/schemas.js
+++ server/schemas/schemas.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose');
 
+const ingredient = new mongoose.Schema({
+	name: String
+});
+
+const ingredientWithQty = new mongoose.Schema({
+	name: ingredient,
+	quantity: String
+});
+
 const recipe = new mongoose.Schema({
 	id: String,
 	name: String,
 	picture: String,
-	ingredients: Array
+	ingredients: [ingredientWithQty]
 });
 
 module.exports = {
-	recipe: recipe
+	recipe: recipe,
+	ingredient: ingredient
 };
